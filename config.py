276799b1c76ac0a4ca7840991293cd718f67a66f
Leanse Sangeerth <leansesection@gmail.com>
2023-12-22 12:27:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
--------------------------------------------------
-   @File Name:     config.py
-   @Author:        Luyao.zhang
-   @Date:          2023/5/16
-   @Description: configuration file
--------------------------------------------------
-"""
 from pathlib import Path
 import sys
 
@@ -31,19 +21,19 @@ SOURCES_LIST = ["Image", "Video", "Webcam"]
 
 # DL model config
 DETECTION_MODEL_DIR = ROOT / 'weights' / 'detection'
-YOLOv8n = DETECTION_MODEL_DIR / "yolov8n.pt"
 YOLOv8s = DETECTION_MODEL_DIR / "yolov8s.pt"
-YOLOv8m = DETECTION_MODEL_DIR / "yolov8m.pt"
+YOLOv8n = DETECTION_MODEL_DIR / "yolov8n.pt"
 YOLOv8l = DETECTION_MODEL_DIR / "yolov8l.pt"
 YOLOv8x = DETECTION_MODEL_DIR / "yolov8x.pt"
 
 DETECTION_MODEL_LIST = [
-    "yolov8n.pt",
     "yolov8s.pt",
-    "yolov8m.pt",
     "yolov8l.pt",
-    "yolov8x.pt"]
+    "yolov8n.pt",
+    "yolov8x.pt"
+    ]
 
 
 OBJECT_COUNTER = None
 OBJECT_COUNTER1 = None
+OBJECT_PRICES = None
